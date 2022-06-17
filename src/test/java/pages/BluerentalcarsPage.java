a4b3b496d36154810d805c72092064220ac783c0
Fatma Öztürk <fatmaozturk2614@gmail.com>
2022-06-17 11:34:15 +0300

    calisma

---
+++ src/test/java/pages/BluerentalcarsPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class BluerentalcarsPage {
+    public BluerentalcarsPage() {
+
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+    @FindBy(xpath="(//a[@role='button'])[1]")
+    public WebElement ilkLogin;
+
+    @FindBy(xpath = "//input[@class='form-control is-invalid'][1]")
+    public WebElement emailAdress;
+    @FindBy(xpath = "//*[text()='Create new user']")
+            public WebElement createNewUserElement;
+
+    @FindBy(xpath = "//input[@class='form-control is-invalid']")
+    public WebElement password;
+
+    @FindBy(xpath = "//button[@type='submit']")
+     public WebElement ikinciLogin;
+
+    @FindBy(tagName = "h1")
+
+    public  WebElement regesterYaziElementi;
+
+    @FindBy(name = "firstName")
+
+    public WebElement firstNameElementi;
+}
---
+++ src/test/java/tests/KullaniciOlusturma.java
@@ -0,0 +1,52 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.BluerentalcarsPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.time.Duration;
+
+public class KullaniciOlusturma {
+
+    @Test
+    public void test1() throws InterruptedException {
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+        BluerentalcarsPage bluerentCarPage = new BluerentalcarsPage();
+        bluerentCarPage.ilkLogin.click();
+        bluerentCarPage.createNewUserElement.click();
+        Assert.assertTrue(bluerentCarPage.regesterYaziElementi.isDisplayed());
+
+
+        Actions actions = new Actions(Driver.getDriver());
+        Faker faker = new Faker();
+        String password = faker.internet().password();
+        String emailAddress = faker.internet().emailAddress();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String expectedName = firstName + " " + lastName;
+        System.out.println("sifre:  " + password);
+        System.out.println("email:  " + emailAddress);
+
+        actions.click(bluerentCarPage.firstNameElementi).
+                sendKeys(firstName + Keys.TAB).
+                sendKeys(lastName + Keys.TAB).
+                sendKeys(faker.phoneNumber().phoneNumber()).
+                sendKeys(faker.address().fullAddress() + Keys.TAB).
+                sendKeys(faker.address().zipCode()+Keys.TAB).
+                sendKeys(faker.internet().emailAddress()+Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(faker.internet().password()+Keys.TAB).
+                sendKeys(faker.internet().password()).
+                sendKeys(Keys.PAGE_DOWN).perform();
+
+    }
+}
